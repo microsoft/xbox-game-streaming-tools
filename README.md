d4bd3ac2305405ef423fc71bfb5e9d0266549b83
Brian Bowman <bbowman@microsoft.com>
2021-07-14 09:55:06 -0700

    Add helper script for generating a touch layout development cert (#15)
    * Add script to help in the creation of development certificates
    
    * Update README to point to scripts and layouts

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 ## Welcome!
 
-Xbox Game Streaming Tools provide schemas and samples to enable game developers that are optimizing their games for Xbox game streaming.  
+Xbox Game Streaming Tools provide schemas and samples to enable game developers that are optimizing their games for Xbox game streaming.
 
 To get started with Xbox game streaming you apply to the ID@Xbox program at http://www.xbox.com/en-us/Developers/id.
 
 To learn more about optimizing your Xbox game for game streaming see the [developer documentation](https://docs.microsoft.com/en-us/gaming/game-streaming/).
 
-
 ## What's included
 
-* Schema representating the capabilities of the [Touch Adaptation Kit](https://docs.microsoft.com/en-us/gaming/game-streaming/ux/touch-adaptation-kit/) that allows for your creation of custom touch layouts for your games.
-* Sample touch adaptation layouts to get started.
+- [Schema](./touch-adaptation-kit/schemas) representating the capabilities of the [Touch Adaptation Kit](https://docs.microsoft.com/en-us/gaming/game-streaming/ux/touch-adaptation-kit/) that allows for your creation of custom touch layouts for your games.
+- [Sample touch adaptation layouts](./touch-adaptation-kit/touch-adaptation-bundles) to get started.
+- [Scripts](./touch-adaptation-kit/scripts) to assist in the development of touch daptation layouts.
 
 ## Contributing
 
-This project welcomes contributions and suggestions.  For more information see [CONTRIBUTING](CONTRIBUTING.md).
+This project welcomes contributions and suggestions. For more information see [CONTRIBUTING](CONTRIBUTING.md).
 
 This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or
 contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
---
+++ touch-adaptation-kit/scripts/Create-SelfSignedCertificate.ps1
@@ -0,0 +1,111 @@
+<#
+    .SYNOPSIS
+    Creates a self signed certificate suitable for use in touch bundle development. Generates both a
+    sideload.pfx and sideload.cer to use. The .pfx is intended to be used on the server while the .cer can be
+    distributed and installed on clients.
+
+    .NOTES
+    Copyright (c) Microsoft Corporation. All rights reserved.
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+    to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+    and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#>
+
+using namespace System.Security.Cryptography.X509Certificates
+
+param
+(
+    [Parameter(HelpMessage = "Host name of the machine the will host the touch bundle server.")]
+    [string]$HostName = $Null,
+ 
+    [Parameter(HelpMessage = "IPAddress of the machine that will host the touch bundle server. If blank, all ip addresses are added.")]
+    [string]$IPAddress = $Null, 
+
+    [Parameter(HelpMessage = "Path to store the generated certificate files in")]
+    [string]$Out = $(Get-Location),
+
+    [Parameter(HelpMessage = "Number of years until the certificate will expire")]
+    [int]$YearsUntilExpiration = 1
+)
+
+# Calculate the IP and DNS names that the certificate should be valid for
+$Subjects = @()
+
+# Always allow connections back to localhost
+$Subjects += "dns=localhost"
+
+# If the hostname was not given, detect it
+if (!$HostName) {
+    $HostName = [System.Net.DNS]::GetHostName()
+}
+
+$Subjects += "dns=${HostName}"
+
+# If the IP address is given explicitly, use that. Otherwise add all IP addresses the machine reports
+if ($IPAddress) {
+    $Subjects += "IPAddress=${IPAddress}"
+}
+else {
+    Get-NetIPAddress |
+    Where-Object { $_.IPv4Address } |
+    ForEach-Object { $Subjects += "IPAddress=$($_.IPAddress)" }
+}
+
+$Extensions = @()
+
+# Basic constraints - the subject may act as a CA
+$Extensions += "2.5.29.19={text}cA=true&pathLength=2"
+
+# Extended key usage - TLS web server authentication
+$Extensions += "2.5.29.37={critical}{text}1.3.6.1.5.5.7.3.1"
+
+# Subject alternate names for the host machine
+$Extensions += "2.5.29.17={critical}{text}$($Subjects -join "&")"
+
+# Generate and export a self signed certificate for use with touch bundle development
+try {
+    $Certificate = New-SelfSignedCertificate `
+        -CertStoreLocation "Cert:\CurrentUser\My" `
+        -FriendlyName "Touch Bundle Development" `
+        -HashAlgorithm "SHA256" `
+        -KeyAlgorithm "RSA" `
+        -KeyLength 2048 `
+        -KeyUsage KeyEncipherment `
+        -NotAfter (Get-Date).AddYears($YearsUntilExpiration) `
+        -NotBefore (Get-Date) `
+        -Subject "CN=Touch Bundle Development" `
+        -TextExtension $Extensions
+ 
+    Write-Output "Generated touch bundle development certificate:"
+    Write-Output "  Name:        $($Certificate.FriendlyName)"
+    Write-Output "  Thumbprint:  $($Certificate.Thumbprint)"
+    Write-Output "  Subjects:    $($Subjects -join "$([Environment]::NewLine)               ")"
+    Write-Output "  Valid On:    $($Certificate.NotBefore)"
+    Write-Output "  Valid Until: $($Certificate.NotAfter)"
+ 
+    $Password = Read-Host "Enter Password to use on .pfx certificate" -AsSecureString
+    Export-PfxCertificate -cert $Certificate -FilePath (Join-Path -Path $Out -ChildPath 'sideload.pfx') -Password $Password
+    Export-Certificate -cert $Certificate -FilePath (Join-Path -Path $Out -ChildPath 'sideload.cer')
+}
+finally {
+    # Erase the keys from the certificate store they were generated in
+    try {
+        if ($Certificate) {
+            $CertStore = [X509Store]::New("MY", [StoreLocation]::CurrentUser)
+            $CertStore.Open([OpenFlags]::ReadWrite + [OpenFlags]::OpenExistingOnly)
+            $CertStore.Remove($Certificate)
+            $CertStore.Close()
+        }
+    }
+    finally {
+        if ($CertStore) {
+            $CertStore.Dispose()
+        }
+    }
+}
